e6106ddcca9938ec16cf6e0be96b5c9bccc48333
Markus Notti <markusnotti@gmail.com>
2016-10-13 14:59:17 -0700

    user has basic model and controller, /signup routes to users#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup', to: 'users#new'
 end
