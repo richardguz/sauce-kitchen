53c96f126512abeafd0bb980340b55c0b946d2d8
Richard Guzikowski <rguzikowski@ucla.edu>
2016-10-25 14:56:13 -0700

    Updated location services, include autocomplete address search bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(username: "testuser1", email: "testuser1@gmail.com", 
+user = User.create!(username: "testuser1", email: "testuser1@gmail.com", 
 						password: "password", password_confirmation: "password")
 
 playlist = Playlist.create!(title: "testPlaylist",
-                user_id: 1)
+                user_id: user.id)
 
 possibleSongs = [132201186]
 5.times do |n|
@@ -28,12 +28,12 @@ end
 end
   
 
-99.times do |n|
+10.times do |n|
 	lat = Random.new.rand(33.906699..34.156027) 
 	lng = Random.new.rand(-118.533987..-118.190153) 
 
   Playlist.create!(title:  "WADDDUPPPP#{n}",
-               user_id: 1,
+               user_id: user.id,
                created_at:  DateTime.now,
                updated_at:  DateTime.now,
                latitude: lat,
