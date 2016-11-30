53e0827ed2cad11716eb7f24e56a20bff9610760
Markus Notti <markusnotti@gmail.com>
2016-11-29 18:49:08 -0800

    adding tsung run script and reformatting load_tests dir

---
+++ load_tests/scripts/tsung_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#change the ip target for the test
+ip=${2//./-}
+sed "s/placeholderIP/ec2-$ip\.us-west-2.compute.amazonaws.com/" $1 > updatedTest.xml
+echo "Changed IP"
+
+#run load tests
+tsung -n -f updatedTest.xml start > tsung_run_output.txt
+echo "Ran Load Tests"
+
+#grab output dir and generate logs
+dir=$(cat tsung_run_output.txt | grep -o -P "(\/home\/ec2-user\/.tsung\/log\/[0-9-]*)")
+echo "$dir"
+cd $dir
+tsung_stats.pl
