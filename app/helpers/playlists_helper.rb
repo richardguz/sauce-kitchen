8445ac3cff381271c74c93fa066619920b176379
Kyle Baker <kylebaker37@gmail.com>
2016-11-30 01:27:25 -0800

    fragment caching on psongs and user player

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,2 +1,11 @@
 module PlaylistsHelper
+	def cache_key_for_psong(psong)
+		"psong-#{psong.id}-#{psong.updated_at}"
+	end
+	def cache_key_for_psong_waiting(psong)
+		"psongwaiting-#{psong.id}-#{psong.updated_at}"
+	end
+	def cache_key_for_user_player(user)
+		"userplayer-#{user.id}-#{user.updated_at}"
+	end
 end
