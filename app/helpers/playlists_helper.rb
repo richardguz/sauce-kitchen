93cdd8f4cdf06363680af927cf52ca5529c5f3fa
Kyle Baker <kylebaker37@gmail.com>
2016-11-30 20:27:59 -0800

    cached user playlists page dunno why

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,4 +1,5 @@
 module PlaylistsHelper
+	# Show playlist page
 	def cache_key_for_psong(psong)
 		"psong-#{psong.id}-#{psong.updated_at}"
 	end
@@ -7,5 +8,15 @@ module PlaylistsHelper
 	end
 	def cache_key_for_user_player(user)
 		"userplayer-#{user.id}-#{user.updated_at}"
+	end
+
+	#User Playlist page
+	def cache_key_for_user_playlists(playlist)
+		"userplaylist-#{playlist.id}-#{playlist.updated_at}"
+	end
+
+	def cache_key_for_user_playlists_table
+		max_stale = @playlists.max_by(&:updated_at).updated_at
+ 		"userplaylisttable-#{@user.id}-#{max_stale}" 
 	end
 end
